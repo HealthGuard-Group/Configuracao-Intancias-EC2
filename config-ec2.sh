277db204331619f451588ff4b5f93ae2d6edc28e
Rfratini <rafael.corso@sptech.school>
2025-10-03 15:31:15 -0300

    Funcionalidade (Shell script) - adicionei no .sh configurações de permicões e instalação do docker

--- config-ec2.sh
+++ config-ec2.sh
@@ -1,8 +1,16 @@
 #!/bin/bash
 
+set -e #caso algum comando falhar ele para o script 
+
+if [ "$EUID" -ne 0 ]; then
+  echo "Erro: Por favor, execute este script como root ou com sudo."
+  exit 1
+fi
 # Criação dos grupos
 echo "+================================+"
 echo "Criando grupos..."
+groupadd health-guard
+echo "Grupo health-guard criado"
 groupadd DBA
 echo "Grupo DBA criado"
 groupadd front-end
@@ -19,16 +27,42 @@ echo ""
 # Criação dos diretórios
 echo "=================================+"
 echo "Criando diretórios..."
-mkdir /home/sistema
-echo "Criando Pasta Sistema"
-mkdir /sistema/back-end
+mkdir -p /home/sistema
+echo "Pasta Sistema criada"
+mkdir -p  /home/sistema/aplicacao-java
 echo "Criando /sistema/back-end"
-mkdir /sistema/front-end
+mkdir -p /home/sistema/aplicacao-python
 echo "Criando /sistema/front-end"
-mkdir /sistema/banco
+mkdir -p /home/sistema/banco
 echo "Criando /sistema/banco"
-echo "Diretórios criados."
-echo "+================================+"
+mkdir -p /home/sistema/site-institucional
+echo "Criando /sistema/site-institucional"
+echo "Diretórios criados em /home/sistema"
+echo "+===================================+"
+
+echo "Atribuindo os diretorios aos seus respectivos grupos..."
+chown :health-guard /home/sistema/
+echo "Permissão do grupo health-guard criada"
+chown -R :DBA /home/sistema/banco/
+echo "Permissão de DBA criada"
+chown -R :back-end /home/sistema/aplicacao-python/
+chown -R :back-end /home/sistema/aplicacao-java/
+echo "Permissão de back-end criada"
+chown -R :front-end /home/sistema/site-institucional/
+echo "Permissão de front-end criada"
 
 echo ""
 echo "+================================+"
+echo " Instalação de arquivos e dependencias..."
+echo "atualizando pacotes"
+apt update
+apt upgrade -y
+echo "Pacotes atualizados com sucesso"
+echo "Instalação do Docker na instancia"
+apt install docker.io 
+echo "Docker instalado com sucesso!"
+systemctl start docker
+systemctl enable docker
+echo "Docker configurado com sucesso!"
+echo "+===============================+"
+echo "Maquina configurada com sucesso!"
